a68eb62e35dc191d4b18f16a2111daa328c4d6bd
peluza <edisonisaza@gmail.com>
2020-09-03 20:02:07 +0000

    3-create_user_replica.sql

--- 0x14-mysql/3-create_user_replica.sql
+++ 0x14-mysql/3-create_user_replica.sql
@@ -1,6 +1,6 @@
 -- Prepares a MySQL server for the project:
-CREATE USER IF NOT EXISTS 'replica_user'@'localhost';
-SET PASSWORD FOR 'replica_user'@'localhost' = PASSWORD('projectcorrection280hbtn');
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'localhost';
+CREATE USER IF NOT EXISTS 'replica_user'@'%';
+SET PASSWORD FOR 'replica_user'@'%' = PASSWORD('projectcorrection280hbtn');
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
