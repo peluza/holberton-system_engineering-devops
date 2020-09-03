b931f21340a1cc6dd8735408681cecc93737780e
peluza <edisonisaza@gmail.com>
2020-09-03 20:01:41 +0000

    4-asignature_replica.sql

--- 0x14-mysql/4-asignature_replica.sql
+++ 0x14-mysql/4-asignature_replica.sql
@@ -1,3 +1,3 @@
 -- Prepares a MySQL server for the project:
-CHANGE MASTER TO MASTER_HOST = '35.190.152.71', MASTER_USER = 'replica_user', MASTER_PASSWORD = 'projectcorrection280hbtn', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 753;
+CHANGE MASTER TO MASTER_HOST = '35.190.152.71', MASTER_USER = 'replica_user', MASTER_PASSWORD = 'projectcorrection280hbtn', MASTER_LOG_FILE = 'mysql-bin.000004', MASTER_LOG_POS = 154;
 FLUSH PRIVILEGES;
