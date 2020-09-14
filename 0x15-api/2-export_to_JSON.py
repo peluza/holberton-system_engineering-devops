6408f0a6a5df3cde67068e3434fa7fce630b1eff
peluza <edisonisaza@gmail.com>
2020-09-14 21:09:14 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,19 +7,20 @@ import sys
 
 if __name__ == "__main__":
     USER_ID = sys.argv[1]
+    passs = "?completed=true&userId="
     url_user = "https://jsonplaceholder.typicode.com/users/" + USER_ID
-    url_id_all = "https://jsonplaceholder.typicode.com/todos/?userId=" + USER_ID
-    url_id_task = "https://jsonplaceholder.typicode.com/todos/?completed=true&userId=" + USER_ID
+    url_id_a = "https://jsonplaceholder.typicode.com/todos/?userId=" + USER_ID
+    url_id_t = "https://jsonplaceholder.typicode.com/todos/" + passs + USER_ID
     req_user = requests.get(url_user).json()
-    req_id_task = requests.get(url_id_task).json()
-    req_id_all = requests.get(url_id_all).json()
+    req_id_t = requests.get(url_id_t).json()
+    req_id_a = requests.get(url_id_a).json()
     EMPLOYEE_NAME = req_user['name']
-    NUMBER_OF_DONE_TASKS = len(req_id_task)
-    TOTAL_NUMBER_OF_TASKS = len(req_id_all)
+    NUMBER_OF_DONE_TASKS = len(req_id_t)
+    TOTAL_NUMBER_OF_TASKS = len(req_id_a)
     USERNAME = req_user.get("username")
     FILE = "{}.json".format(USER_ID)
     list_json = []
-    for task in req_id_all:
+    for task in req_id_a:
         TASK_COMPLETED_STATUS = task.get("completed")
         TASK_TITLE = task.get("title")
         dic = {'task': TASK_TITLE,
