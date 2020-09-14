83a7e8d98e92e26bfa2760b54385fa91ffacdc40
peluza <edisonisaza@gmail.com>
2020-09-14 21:03:03 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,18 +6,17 @@ import sys
 
 if __name__ == "__main__":
     USER_ID = sys.argv[1]
+    passs = "?completed=true&userId="
     url_user = "https://jsonplaceholder.typicode.com/users/" + USER_ID
-    url_id_all = "https://jsonplaceholder.typicode.com/\
-        todos/?userId=" + USER_ID
-    url_id_task = "https://jsonplaceholder.typicode.com/\
-        todos/?completed=true&userId=" + USER_ID
+    url_id_a = "https://jsonplaceholder.typicode.com/todos/?userId=" + USER_ID
+    url_id_t = "https://jsonplaceholder.typicode.com/todos/" + passs + USER_ID
     req_user = requests.get(url_user).json()
-    req_id_task = requests.get(url_id_task).json()
-    req_id_all = requests.get(url_id_all).json()
+    req_id_t = requests.get(url_id_t).json()
+    req_id_a = requests.get(url_id_a).json()
     EMPLOYEE_NAME = req_user['name']
-    NUMBER_OF_DONE_TASKS = len(req_id_task)
-    TOTAL_NUMBER_OF_TASKS = len(req_id_all)
-    print("Employee {} is done with tasks({}/{}):\
-        ".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in req_id_task:
+    NUMBER_OF_DONE_TASKS = len(req_id_t)
+    TOTAL_NUMBER_OF_TASKS = len(req_id_a)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for task in req_id_t:
         print("	 {}".format(task.get("title")))
