9da29907c3e5a972ebf291c1a4ddf011bc8b3949
peluza <edisonisaza@gmail.com>
2020-09-17 19:27:29 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,14 @@ import requests
 
 
 def top_ten(subreddit):
+    """top_ten
 
+    Args:
+        subreddit (str): user the reddit
+
+    Returns:
+        str:  prints the titles of the first 10 hot
+    """
     agent_user = {'User-agent': 'TrickyChart131'}
     url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
     get_req = requests.get(url, headers=agent_user)
