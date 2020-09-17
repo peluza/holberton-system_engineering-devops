55c6b943ed79a937dab6862f41d294a3801a009f
peluza <edisonisaza@gmail.com>
2020-09-17 19:26:01 +0000

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Top Ten
+    """
+import requests
+
+
+def top_ten(subreddit):
+
+    agent_user = {'User-agent': 'TrickyChart131'}
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    get_req = requests.get(url, headers=agent_user)
+    get_subs = get_req.json()
+    if get_req.status_code == 404:
+        return (0)
+    else:
+        dict_p = get_subs["data"]["children"]
+        j = 0
+        for j in range(10):
+            for i in dict_p:
+                print(i["data"]["title"])
