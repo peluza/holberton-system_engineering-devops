df539211cd3ccfc54e1a20b2c20062b5b987fbf7
peluza <edisonisaza@gmail.com>
2020-09-30 00:32:33 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-#change the content in the file wp-settings, the verification the information
+#the verification the information
 #is change the flie wp-config.php in option debug in true for identification error
+#change the content in the file wp-settings for error identificatiion in debug
 exec {'fix-wordpress':
   provider => shell,
   command  => 'sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php',
