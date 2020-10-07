b2c2592fb552c4a371a611db25861d8093664d28
peluza <edisonisaza@gmail.com>
2020-10-07 16:49:36 -0500

    run_srver.sh

--- 0x1A-application_server/run_srver.sh
+++ 0x1A-application_server/run_srver.sh
@@ -6,4 +6,5 @@ tmux new-session -d 'gunicorn --bind 0.0.0.0:5001 web_flask.6-number_odd_or_even
 cd ~/AirBnB_clone_v3
 tmux new-session -d 'gunicorn --bind 0.0.0.0:5002 api.v1.app:app'
 cd ~/AirBnB_clone_v4
-tmux new-session -d 'gunicorn --bind 0.0.0.0:5003 web_dynamic.2-hbnb:app'
+tmux new-session -d 'HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5003 gunicorn --bind 0.0.0.0:5003 web_dynamic.2-hbnb:app'
+
